10a3b0e290a25792cabfdd89a016052088ab1f9f
rafalkbflabs <rafal@bflabs.com>
2022-10-03 10:40:40 +0200

    Add formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import 'antd/dist/antd.min.css';
 import './App.css';
+import { Layout } from 'antd';
 
 function App() {
+  const { Header, Content } = Layout;
+
   return (
-    <div className="">
-      <header className="">
-        Header
-      </header>
-    </div>
+    <Layout>
+      <Header className="header">HEADER</Header>
+      <Layout>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
